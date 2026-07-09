12f0747e8fbaf9adf1b8685d451f742c922945bb
Dguto9 <dillongutowski@gmail.com>
2026-07-08 23:27:56 -0400

    Update trait

--- src/dag.rs
+++ src/dag.rs
@@ -7,11 +7,11 @@ pub trait Dag: Default + IndexMut<Self::NodeId, Output = Self::NodeWeight> {
     fn neighbors(&self, node: Self::NodeId) -> impl Iterator<Item = Self::NodeId>;
     fn find_nodes(
         &self,
-        func: impl Fn(Self::NodeId, Self::NodeWeight) -> bool,
+        func: impl FnMut(Self::NodeId, &Self::NodeWeight) -> bool,
     ) -> impl Iterator<Item = Self::NodeId>;
     fn find_node(
         &self,
-        func: impl Fn(Self::NodeId, Self::NodeWeight) -> bool,
+        func: impl FnMut(Self::NodeId, &Self::NodeWeight) -> bool,
     ) -> Option<Self::NodeId>;
     fn get(&self, id: Self::NodeId) -> Option<&Self::NodeWeight>;
     fn get_mut(&mut self, id: Self::NodeId) -> Option<&mut Self::NodeWeight>;
