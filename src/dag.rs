0992f899b157e4cc949dd6a7c3312706b8289e41
Omnikar <omnikar5@gmail.com>
2026-07-09 02:20:28 -0400

    Take NodeId by reference

--- src/dag.rs
+++ src/dag.rs
@@ -1,6 +1,6 @@
 use std::ops::IndexMut;
 
-pub trait Dag: Default + IndexMut<Self::NodeId, Output = Self::NodeWeight> {
+pub trait Dag: Default + for<'id> IndexMut<&'id Self::NodeId, Output = Self::NodeWeight> {
     type NodeWeight;
     type NodeId;
     /// Return forward-neighbors of this node in no particular order
@@ -8,13 +8,13 @@ pub trait Dag: Default + IndexMut<Self::NodeId, Output = Self::NodeWeight> {
     fn neighbors_back(&self, node: Self::NodeId) -> impl Iterator<Item = Self::NodeId>;
     fn find_nodes(
         &self,
-        func: impl FnMut(Self::NodeId, &Self::NodeWeight) -> bool,
+        func: impl FnMut(&Self::NodeId, &Self::NodeWeight) -> bool,
     ) -> impl Iterator<Item = Self::NodeId>;
     fn find_node(
         &self,
-        func: impl FnMut(Self::NodeId, &Self::NodeWeight) -> bool,
+        func: impl FnMut(&Self::NodeId, &Self::NodeWeight) -> bool,
     ) -> Option<Self::NodeId>;
-    fn get(&self, id: Self::NodeId) -> Option<&Self::NodeWeight>;
-    fn get_mut(&mut self, id: Self::NodeId) -> Option<&mut Self::NodeWeight>;
+    fn get(&self, id: &Self::NodeId) -> Option<&Self::NodeWeight>;
+    fn get_mut(&mut self, id: &Self::NodeId) -> Option<&mut Self::NodeWeight>;
     // what other functions go here?
 }
