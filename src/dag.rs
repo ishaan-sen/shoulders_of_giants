4335c28444ec7af5b7d954a29e9c4ead09b277e8
Dguto9 <dillongutowski@gmail.com>
2026-07-10 13:06:53 -0400

    Implement LCA

--- src/dag.rs
+++ src/dag.rs
@@ -1,5 +1,5 @@
 use crate::Paper;
-use std::{ops::IndexMut, rc::Rc};
+use std::{collections::HashSet, ops::IndexMut, rc::Rc};
 
 pub trait Dag: for<'id> IndexMut<&'id Self::NodeId, Output = Self::NodeWeight> {
     type NodeWeight;
@@ -31,6 +31,42 @@ fn find_by_id(dag: &impl Dag<NodeWeight = Paper>, id: Rc<str>) -> Option<&Paper>
 // fn earliest_common_descendant(dag: &impl Dag<NodeWeight = Paper>) -> impl Iterator<Item = Paper> {
 //     todo!()
 // }
-// fn last_common_ancestor(dag: &impl Dag<NodeWeight = Paper>) -> impl Iterator<Item = Paper> {
-//     todo!()
-// }
+fn last_common_ancestor<T: Eq + std::hash::Hash + Clone>(
+    dag: &impl Dag<NodeWeight = Paper, NodeId = T>,
+    a: T,
+    b: T,
+) -> impl Iterator<Item = &Paper> {
+    let mut visited_a: HashSet<T> = HashSet::new();
+    let mut visited_b: HashSet<T> = HashSet::new();
+    let mut check_a: HashSet<T> = dag.neighbors(a).collect();
+    let mut check_b: HashSet<T> = dag.neighbors(b).collect();
+    let mut resulting: HashSet<T> = HashSet::new();
+    loop {
+        if check_a.is_empty() && check_b.is_empty() {
+            break;
+        }
+        if check_a.is_disjoint(&visited_b) && check_b.is_disjoint(&visited_a) {
+            let temp: HashSet<T> = check_a
+                .iter()
+                .cloned()
+                .map(|x| dag.neighbors(x))
+                .flatten()
+                .collect();
+            visited_a.extend(check_a);
+            check_a = temp;
+            let temp: HashSet<T> = check_b
+                .iter()
+                .cloned()
+                .map(|x| dag.neighbors(x))
+                .flatten()
+                .collect();
+            visited_b.extend(check_b);
+            check_b = temp;
+        } else {
+            resulting.extend(check_a.intersection(&visited_b).cloned());
+            resulting.extend(check_b.intersection(&visited_a).cloned());
+            break;
+        }
+    }
+    resulting.into_iter().map(|x| &dag[&x])
+}
