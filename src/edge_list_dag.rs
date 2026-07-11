4d745930fc05dd0f53869b574c53aa10065d9398
Ethan Yin <ethanyin0109@gmail.com>
2026-07-11 15:50:20 -0400

    convert node_map to local variable and remove unused get_connected_edges

--- src/edge_list_dag.rs
+++ src/edge_list_dag.rs
@@ -1,13 +1,11 @@
-use crate::Paper;
 use crate::dag::Dag;
+use crate::Paper;
 use std::collections::HashMap;
 use std::ops::{Index, IndexMut};
-use std::rc::Rc;
 
 pub struct EdgeListDag<T> {
     pub nodes: Vec<T>,
     pub edges: Vec<(usize, usize)>,
-    pub node_map: HashMap<Rc<str>, usize>,
 }
 
 impl<T> EdgeListDag<T> {
@@ -15,29 +13,11 @@ impl<T> EdgeListDag<T> {
         Self {
             nodes: Vec::new(),
             edges: Vec::new(),
-            node_map: HashMap::new(),
         }
     }
 
     pub fn add_edge(&mut self, citer_idx: usize, cited_idx: usize) {
         self.edges.push((citer_idx, cited_idx));
-    }
-
-    pub fn get_connected_edges(&self, target_id: &str) -> Option<(Vec<&T>, Vec<&T>)> {
-        let &target_idx = self.node_map.get(target_id)?;
-
-        let mut incoming = Vec::new();
-        let mut outgoing = Vec::new();
-
-        for &(citer, cited) in &self.edges {
-            if citer == target_idx {
-                outgoing.push(&self.nodes[cited]);
-            } else if cited == target_idx {
-                incoming.push(&self.nodes[citer]);
-            }
-        }
-
-        Some((incoming, outgoing))
     }
 }
 
@@ -45,6 +25,8 @@ impl FromIterator<crate::CSVRecord> for EdgeListDag<Paper> {
     fn from_iter<T: IntoIterator<Item = crate::CSVRecord>>(iter: T) -> Self {
         let mut dag = EdgeListDag::new();
         let records: Vec<_> = iter.into_iter().collect();
+        
+        let mut node_map = HashMap::new();
 
         for rec in &records {
             let paper = Paper {
@@ -53,14 +35,14 @@ impl FromIterator<crate::CSVRecord> for EdgeListDag<Paper> {
                 abstract_text: rec.abstract_text.clone(),
             };
             let idx = dag.nodes.len();
-            dag.node_map.insert(paper.id.clone(), idx);
+            node_map.insert(paper.id.clone(), idx);
             dag.nodes.push(paper);
         }
 
         for rec in &records {
-            if let Some(&citer_idx) = dag.node_map.get(&rec.id) {
+            if let Some(&citer_idx) = node_map.get(&rec.id) {
                 for refr in &rec.references {
-                    if let Some(&cited_idx) = dag.node_map.get(refr) {
+                    if let Some(&cited_idx) = node_map.get(refr) {
                         dag.add_edge(citer_idx, cited_idx);
                     }
                 }
@@ -91,20 +73,24 @@ impl<T> Dag for EdgeListDag<T> {
 
     fn neighbors(&self, node: &Self::NodeId) -> impl Iterator<Item = Self::NodeId> {
         let target = *node;
-        self.edges.iter().filter_map(
-            move |&(citer, cited)| {
-                if citer == target { Some(cited) } else { None }
-            },
-        )
+        self.edges.iter().filter_map(move |&(citer, cited)| {
+            if citer == target {
+                Some(cited)
+            } else {
+                None
+            }
+        })
     }
 
     fn neighbors_back(&self, node: &Self::NodeId) -> impl Iterator<Item = Self::NodeId> {
         let target = *node;
-        self.edges.iter().filter_map(
-            move |&(citer, cited)| {
-                if cited == target { Some(citer) } else { None }
-            },
-        )
+        self.edges.iter().filter_map(move |&(citer, cited)| {
+            if cited == target {
+                Some(citer)
+            } else {
+                None
+            }
+        })
     }
 
     fn find_nodes(
